bd89538a1e3e81561418dce7905345258a982fd0
Maddalena Lischetti <maddalena.lischetti@gmail.com>
2020-05-01 18:38:19 +0200

    aggiunto colonna distretto con shapefile Mascaretti

--- script/add_districts_to_tracks.R
+++ script/add_districts_to_tracks.R
@@ -6,14 +6,14 @@ library(rgdal)
 library(sp)
 
 
-
+#########################################################################################
 my_map <- readOGR( 
   dsn= paste0(getwd(),"/DistrictMap") , 
   layer="Maputo_Quartieri",
   verbose=FALSE
 )
 
-df_tracks_districts <- df_tracks 
+df_tracks_districts<-df_tracks
 dat <- data.frame( Longitude=df_tracks_districts$lng, Latitude=df_tracks_districts$lat )
 
 
@@ -27,5 +27,29 @@ district <- over(dat,my_map)
 df_tracks_districts <- cbind(df_tracks, c(district))
 colnames(df_tracks_districts)[19] <- c("district_id")
 colnames(df_tracks_districts)[20] <- c("district_name")
+
+
+############################################################################################
+# Usando i distretti nello shapefile di Mascaretti
+
+district_map<- readOGR( 
+  dsn= paste0(getwd(),"/DistrictMap") , 
+  layer="HomogenousDivisionOK",
+  verbose=FALSE
+)
+
+
+
+points <- data.frame( east=df_tracks$Easting, north=df_tracks$Northing )
+coordinates(points) <- ~ east + north
+proj4string(points) <- proj4string(district_map)
+
+district <- over(points,district_map)
+
+#add the columns with the district id
+df_tracks_districts <- cbind(df_tracks, c(district))
+colnames(df_tracks_districts)[colnames(df_tracks_districts)=="fid"] <- "district_id"
+head(df_tracks_districts)
+
 
 
