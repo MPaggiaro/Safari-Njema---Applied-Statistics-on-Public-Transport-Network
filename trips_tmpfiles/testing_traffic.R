1260dd1d0d6b5d568f07ce5de6a1b5c867e03c03
Matteo Paggiaro <matteo.paggiaro@gmail.com>
2020-05-10 17:07:02 +0200

    new

--- trips_tmpfiles/testing_traffic.R
+++ trips_tmpfiles/testing_traffic.R
@@ -31,6 +31,11 @@ tracks_3 <- df_tracks[df_tracks$Zone_ID == 3,]
 # over the days, count:
 tracks_3_night <- tracks_3[tracks_3$hour <= 5,]
 tracks_3_morning <- tracks_3[tracks_3$hour >=6 & tracks_3$hour <=9,]
+tracks_3_midday <-tracks_3[tracks_3$hour >=10 & tracks_3$hour <=14,]
+tracks_3_anoon <-tracks_3[tracks_3$hour >=15 & tracks_3$hour <=18,]
+tracks_3_evening <-tracks_3[tracks_3$hour >=19 & tracks_3$hour <=24,]
+
+
 
 N = 180-30
 morning = rep(0,N)
@@ -53,6 +58,54 @@ for (i in seq(31,180)){
   night[i-30] <- dim(tracks_3_n_daily)[1]
 }
 
-hist(night, breaks = 20)
-hist ( (night - mean(night)) /sd(night), breaks = 20 )
-plot(night[1:50])
+# hist(night, breaks = 20)
+# hist ( (night - mean(night)) /sd(night), breaks = 20 )
+# plot(night[1:50])
+
+morning = rep(0,N)
+# unique:
+for (i in seq(31,180)){
+  tracks_3_m_daily <- tracks_3_morning[tracks_3_morning$day==i,]
+  tracks_3_m_daily <- tracks_3_m_daily[!duplicated(tracks_3_m_daily$id),]
+  morning[i-30] <- dim(tracks_3_m_daily)[1]
+}
+
+midday = rep(0,N)
+# unique:
+for (i in seq(31,180)){
+  tracks_3_n_daily <- tracks_3_midday[tracks_3_midday$day==i,]
+  tracks_3_n_daily <- tracks_3_n_daily[!duplicated(tracks_3_n_daily$id),]
+  midday[i-30] <- dim(tracks_3_n_daily)[1]
+}
+
+
+anoon = rep(0,N)
+# unique:
+for (i in seq(31,180)){
+  tracks_3_n_daily <- tracks_3_anoon[tracks_3_anoon$day==i,]
+  tracks_3_n_daily <- tracks_3_n_daily[!duplicated(tracks_3_n_daily$id),]
+  anoon[i-30] <- dim(tracks_3_n_daily)[1]
+}
+
+
+evening = rep(0,N)
+# unique:
+for (i in seq(31,180)){
+  tracks_3_n_daily <- tracks_3_evening[tracks_3_evening$day==i,]
+  tracks_3_n_daily <- tracks_3_n_daily[!duplicated(tracks_3_n_daily$id),]
+  evening[i-30] <- dim(tracks_3_n_daily)[1]
+}
+
+total = night + morning + midday + anoon + evening
+
+# frequencies over the total day traffic:
+fr_night = night/total
+fr_morning = morning/total
+fr_midday = midday/total
+fr_anoon = anoon/total
+fr_evening = evening/total
+
+# saving the data in a dataset:
+traffic.time.slots <- data.frame(night,fr_night,morning,fr_morning,midday,fr_midday,anoon,fr_anoon,evening,fr_evening,total)
+
+
