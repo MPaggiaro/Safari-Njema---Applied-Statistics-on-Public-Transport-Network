a8048e08b23457e34472a39dacb428cec4e86cf0
Samuele Milanesi <samuele.milanesi@gmail.com>
2020-07-04 09:58:32 +0200

    completato clustering

--- script/tragitti.R
+++ script/tragitti.R
@@ -3,28 +3,28 @@
 
 temp<- unique(df_tracks$day * 100000 + df_tracks$journey_id)
 
-
 df_tragitto<-df_tracks
 df_tragitto<-cbind(df_tragitto, trip_id= df_tracks$day * 100000 + df_tracks$journey_id )
 
 indicini = rep(TRUE,n)
 for(i in 2:(n-1)){
   
-  if((df_tragitto$stop_here[i]==1) &(df_tragitto$trip_id[i-1]==df_tragitto$trip_id[i]) & (df_tragitto$trip_id[i]==df_tragitto$trip_id[i+1])){
+    if((df_tragitto$stop_here[i]==1) &(df_tragitto$trip_id[i-1]==df_tragitto$trip_id[i]) & (df_tragitto$trip_id[i]==df_tragitto$trip_id[i+1])){
     
     indicini[i] = !((df_tragitto$stop_here[i-1]==1) & (df_tragitto$stop_here[i+1]==1))
-    
-  }
+  
+    }
 }
 
-df_tragiotti = df_tragitto[indicini,]
+df_tragitto = df_tragitto[indicini,]
 
-df_tragitto = df_tragiotto
+
 
 
 n<- dim(df_tragitto)[1]
 delta_t<- df_tragitto$timestamp[2:n ]- df_tragitto$timestamp[1:n-1]
 delta_t<-c(0,delta_t)
+> diss<-dist(df_tragitto_shuffled[1210001: dim(df_tragitto_shuffled)[1], c(3,4)])
 
 df_tragitto<-cbind(df_tragitto, delta_t)
 
@@ -67,13 +67,15 @@ for ( i in seq(n+1,dim(df_tragitto_no)[1], by = n )){
   clust3<-c(clust3, temp)
 }
 
+ultimo = seq(n+1,dim(df_tragitto_no)[1], by=n)
+ultimo = ultimo[length(ultimo)]
 
-diss<-dist(df_tragitto_shuffled[1210001: dim(df_tragitto_shuffled)[1], c(3,4)])
+diss<-dist(df_tragitto_shuffled[ultimo: dim(df_tragitto_shuffled)[1], c(3,4)])
 clust<-hclust(diss, 'ward.D2')
 
 temp<-cutree(clust, k=3)
-is1= temp[which.min(df_tragitto_shuffled[1210001: dim(df_tragitto_shuffled)[1],]$vi)]
-is3= temp[which.max(df_tragitto_shuffled[1210001: dim(df_tragitto_shuffled)[1],]$vi)]
+is1= temp[which.min(df_tragitto_shuffled[ultimo: dim(df_tragitto_shuffled)[1],]$vi)]
+is3= temp[which.max(df_tragitto_shuffled[ultimo: dim(df_tragitto_shuffled)[1],]$vi)]
 
 temp[which(temp==is3)] =3
 temp[which(temp==is1)] =1
@@ -88,6 +90,7 @@ plot(df_tragitto_shuffled[, c(3,4)], pch=18, cex=0.05,  col= clust3+1)
 
 table(clust3)
 
+backupino = df_tragitto_shuffled
 ##Assegno un cluster
 
 df_tragitto_shuffled<-cbind(df_tragitto_shuffled, clust3)
@@ -96,12 +99,34 @@ df_tragitto_shuffled<-cbind(df_tragitto_shuffled, clust3)
 
 df_tragitto_clustered<-df_tragitto_shuffled[order(df_tragitto_shuffled$idx), ]
 
-#Cancello la colonna deglo indici
+df_tragitto_clustered<-df_tragitto_clustered[,-5]
+data_no_walk = df_tragitto_clustered[which(df_tragitto_clustered$clust3!=1),]
 
-df_tragitto_clustered<- df_tragitto_clustered[, -c(5)]
+
+for (i in unique(data_no_walk$trip_id)){
+  tmp = mean(data_no_walk[which(data_no_walk$trip_id==i),]$clust3)
+  if (tmp>=2.5){
+    data_no_walk[which(data_no_walk$trip_id==i),]$clust3=3}
+  else{
+    data_no_walk[which(data_no_walk$trip_id==i),]$clust3=2}
+}
+
+
+
+idx = ifelse(df_tragitto_clustered$clust3==1,FALSE,TRUE)
+
+backuppone = df_tragitto_clustered
+df_tragitto_clustered=backuppone
+df_tragitto_clustered[idx,]=data_no_walk
+
+df_arch = df_tragitto_clustered
+
 df_tragitto_clustered[df_tragitto_clustered$clust3==1, c(5) ]<- 'walk'
 df_tragitto_clustered[df_tragitto_clustered$clust3==2, c(5)]<- 'chapas'
 df_tragitto_clustered[df_tragitto_clustered$clust3==3, c(5)]<- 'car'
-colnames(df_tragitto_clustered)[5]<- 'typeOfJourney'
+colnames(df_tragitto_clustered)[5]<- 'clust3'
 
 
+x11()
+
+plot(df_arch[, c(3,4)], pch=18, cex=0.05,  col=df_arch$clust3+1)
